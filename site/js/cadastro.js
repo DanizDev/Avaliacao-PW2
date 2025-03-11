64f943af8e11cccc29b48e220470db9bcac06d5b
JOSE DANIEL ALICRIM BRAGA <JOSE_4857@uirapuru.br>
2025-03-10 21:40:53 -0300

    Avaliação de PW2

---
+++ site/js/cadastro.js
@@ -0,0 +1,32 @@
+function cadastrar(event){
+
+    event.preventDefault();
+
+    var email = document.getElementById('email').value;
+    var usuario = document.getElementById('usuario').value; 
+    var senha = document.getElementById('senha').value;
+
+    if(usuario == 'admin' && senha == 'admin1' && email == 'daniz.dev@hotmail.com'){
+
+        Swal.fire({
+            title: 'Cadastro realizado com sucesso!!!',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        }).then(() => {
+           setTimeout(() => {
+            location.href = "./index.html";
+           }, 100);
+        });  
+        
+    }else{
+        Swal.fire({
+            title: 'Login não realizado',
+            text: 'Dados incorretos!!!!',
+            icon: 'error',
+            confirmButtonText: 'Tente Novamente!'
+        });
+        
+    }
+
+
+}
